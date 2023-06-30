1af39f9b48c1717e847e1d4ce40fe04466515ffd
Stellas14 <114410155+Stellas14@users.noreply.github.com>
2023-06-29 21:13:21 -0300

    texto do que foi feito nessa modificacao

---
+++ tecProgAtv.java
@@ -0,0 +1,25 @@
+import java.util*
+
+public class Java{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor:");
+        int n1 = sc.nextInt();
+
+        System.out.println("Digite o segundo valor:");
+        int n2 = sc.nextInt();
+
+        System.out.println("Digite o terceiro valor:");
+        int n3 = sc.nextInt();
+
+        System.out.println("");
+
+    }
+}
+        // se n1 > n2 and n3 = deixar ele na ultima casa 
+
+
+
+ /*Faça um programa na Linguagem Java que leia 3 (três) valores inteiros
+       apresente os 3 números em ordem crescente. */
